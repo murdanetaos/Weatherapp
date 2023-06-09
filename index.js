f75af4410e9331d5596f15486469082eef613b1f
murdanetaos <119532654+murdanetaos@users.noreply.github.com>
2023-06-09 14:06:43 -0400

    add

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const https = require("https");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+const express = require("express"); //includes the express package from node
+const app = express(); //creates a express application
+const bodyParser = require("body-parser");
+const https = require("https"); //include the https package from node
+const { json } = require("stream/consumers");
+
+//Here we are creating our roiute for URL to page.html
+app.use(bodyParser.urlencoded({ extended: true })); // allows us to parse our url code
 app.get("/", function (req, res) {
+  //we are aceessing the html file and creating a function for a request
   res.sendFile(__dirname + "/page.html");
 });
+
+//Here we will implement our API CALL to our URL
+
 app.post("/", function (req, res) {
   const cityName = req.body.cityName;
-  const url =  `https://api.openweathermap.org/data/2.5/weather?lat=35.227085&lon=-80.843124&appid=7732788075736165a03e6ee4fde2240e&units=imperial`;
-
-  https.get(url, function (response) {
+  const state = req.body.state;
+  const geourl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${state},US&limit=&appid=7732788075736165a03e6ee4fde2240e`;
+  https.get(geourl, function (response) {
     response.on("data", function (data) {
-      const jsondata = JSON.parse(data);
-      const temp = jsondata.main.temp;
-      const des = jsondata.weather[0].description;
-      const icon = jsondata.weather[0].icon;
-      const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      res.write(`<h1>The temp in ${cityName} is ${temp} degrees</h1>`);
-      res.write(`<p>The weather description is ${des} </p>`);
-      res.write("<img src=" + imageurl + ">");
-      res.send();
+      const geodata = JSON.parse(data)[0];
+      console.log(geodata);
+
+      const lat = geodata.lat;
+      const long = geodata.lon;
+      const weatherurl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=7732788075736165a03e6ee4fde2240e`;
+      https.get(weatherurl, function (weatherResponse) {
+        weatherResponse.on("data", function (weatherdata) {
+          const weatherJson = JSON.parse(weatherdata);
+          console.log(weatherJson);
+          const temp = weatherJson.main.temp;
+          const des = weatherJson.weather[0].description;
+          const icon = weatherJson.weather[0].icon;
+          const imageurl =
+            "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+          res.write(
+            `<div>
+            <head> <title>Weather Application Data</title> </head>
+            <h1>The temp in  ${cityName}, ${geodata.state} is ${temp}</h1>
+            <p id = "description" >The weather description is ${des}</p>
+            <img src ="${imageurl}">
+            </div>
+            `
+          );
+          res.send();
+        });
+      });
     });
   });
 });
